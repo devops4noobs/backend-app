852bef2674639b05eaad1f352171c247fe667b2f
haldanciprian <haldanciprian@yahoo.com>
2024-03-03 22:24:57 +0200

    first deploy

---
+++ src/main/java/com/spring/spring/UserController.java
@@ -0,0 +1,38 @@
+package com.spring.spring;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin(origins = "http://localhost:3000")
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    @PostMapping("/add")
+    public User createUser(@RequestBody User user) {
+        return userRepository.save(user);
+    }
+
+    public UserRepository getUserRepository() {
+      return userRepository;
+    }
+
+    public void setUserRepository(UserRepository userRepository) {
+      this.userRepository = userRepository;
+    }
+}
