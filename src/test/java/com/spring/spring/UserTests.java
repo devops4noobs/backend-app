3801374bef87053a40341d0e2f21f3af94851ef3
ciprian haldan <haldanciprian@gmail.com>
2024-03-08 21:05:15 +0200

    fix

--- src/test/java/com/spring/spring/UserTests.java
+++ src/test/java/com/spring/spring/UserTests.java
@@ -1,9 +1,15 @@
 package com.spring.spring;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.argThat;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -16,16 +22,26 @@ public class UserTests {
     @Autowired
     private MockMvc mockMvc;
 
+    @MockBean
+    private UserRepository userRepositoryMock;
+
     @Test
     public void testAddEndpoint() throws Exception {
-        String requestBody = "{\"name\":\"test\", \"age\":34}";
+      User user = new User();
+      user.setName("test");
+      user.setAge(34);
+      String requestBody = "{\"name\":\"test\", \"age\":34}";
+
+      when(userRepositoryMock.save(any(User.class))).thenReturn(user);
 
         mockMvc.perform(MockMvcRequestBuilders.post("/api/add")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(requestBody))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                // Add more assertions if needed
-                .andReturn();
+                .andExpect(MockMvcResultMatchers.jsonPath("$.name").value("test"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.age").value(34));
+
+                verify(userRepositoryMock).save(argThat(savedUser -> savedUser.getName().equals("test") && savedUser.getAge() == 34));
     }
 
 
