046b927b3f64a74cc2adf057eee5c6ce20435f5f
ciprian haldan <haldanciprian@gmail.com>
2024-03-08 20:15:23 +0200

    add unit test

--- src/test/java/com/spring/spring/UserTests.java
+++ src/test/java/com/spring/spring/UserTests.java
@@ -1,4 +1,32 @@
 package com.spring.spring;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class UserTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testAddEndpoint() throws Exception {
+        String requestBody = "{\"name\":\"test\", \"age\":34}";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/add")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(requestBody))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                // Add more assertions if needed
+                .andReturn();
+    }
+
+
 }
